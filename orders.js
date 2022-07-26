31c2f8c07e13110e5a32ea37524616b653494d4f
Mr-00-Tuzki <100724101+Mr-00-Tuzki@users.noreply.github.com>
2022-07-26 13:19:40 +0530

    uploaded

---
+++ orders.js
@@ -0,0 +1,50 @@
+const orders=[
+{
+    productName:'Foldable Mini Drone',
+    productNumber:'85361',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName:'Lavender KF10 drone',
+    productNumber:'84965',
+    paymentStatus:'Refunded',
+    shipping:'Declined'
+},
+{
+    productName:'Mist Mini Drone',
+    productNumber:'32569',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName:'Karlos Major Drone Amp 5',
+    productNumber:'58964',
+    paymentStatus:'Paid',
+    shipping:'Delivered'
+},
+{
+    productName:'Waking Mini Drone',
+    productNumber:'848132',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName:'Tombo mini Drone',
+    productNumber:'78469',
+    paymentStatus:'Refunded',
+    shipping:'Pending'
+},
+{
+    productName:'Foldable Major Drone',
+    productNumber:'85362',
+    paymentStatus:'Due',
+    shipping:'Pending'
+},
+{
+    productName:'Flexible Drone',
+    productNumber:'84859',
+    paymentStatus:'Paid',
+    shipping:'Delivered'
+}
+]
